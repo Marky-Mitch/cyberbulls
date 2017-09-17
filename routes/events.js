141e92e387f2da19dab26099bdcddc282bff182e
Marky-Mitch <mjm2g12@soton.ac.uk>
2017-09-17 20:25:33 +0100

    Initial static website functionality

--- routes/events.js
+++ routes/events.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cyber Bulls' });
+  
+  //connect to db and find list of events
+  res.render('events', { title: 'Events' });
 });
 
 module.exports = router;
